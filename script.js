3f2368084114cf819830e2f6fdd2233c157a401c
cmcau <cmcclellau@gmail.com>
2024-05-13 06:11:12 +1000

    Added option to download preferences file directly

--- script.js
+++ script.js
@@ -11,14 +11,10 @@ document.getElementById("colorForm").addEventListener("submit", function(event) 
     alert("Please paste a URL to convert to your color palette.");
     return;
   }
-//  let colors = input.split("/").pop().split("-");
+
   let colors;
 
   if (input.includes('coolors.co')) {
-//    const regex = /(?:\/|\-)([0-9A-Fa-f]{6})(?!\w)/g;
-//    colors = input.match(regex); // Assign value here
-    // Remove hyphens from color codes
-//    colors = colors.map(color => color.replace('-', ''));
     colors = input.split("/").pop().split("-");
   }
   // Check if input contains a ColorHunt URL
@@ -59,4 +55,66 @@ document.getElementById("copyButton").addEventListener("click", function() {
   textarea.select();
   document.execCommand("copy");
   alert("Copied to clipboard!");
+});
+
+// Create a link element
+const link = document.createElement('a');
+
+document.getElementById("generateBtn").addEventListener("click", function() {
+  let input = document.getElementById("colorInput").value;
+  let paletteName = document.getElementById("paletteNameInput").value.trim();
+
+  if (!input) {
+    alert("Please paste a URL to convert to your color palette.");
+    return;
+  }
+
+  let colors; // Declare colors variable here
+
+  if (input.includes('coolors.co')) {
+    colors = input.split("/").pop().split("-");
+  }
+  // Check if input contains a ColorHunt URL
+  else if (input.includes('colorhunt.co')) {
+    const regex = /palette\/([0-9A-Za-z]{24})/;
+    const match = input.match(regex);
+    if (match && match[1]) {
+      const paletteID = match[1];
+      // Split the palette ID into four 6-character color codes
+      colors = [
+        paletteID.substr(0, 6),
+        paletteID.substr(6, 6),
+        paletteID.substr(12, 6),
+        paletteID.substr(18, 6)
+      ];
+    } else {
+      alert("Invalid ColorHunt URL format.");
+      return;
+    }
+  }
+
+  if (!colors) {
+    alert("Unable to extract colors from the provided URL.");
+    return;
+  }
+
+  // Generate XML content dynamically
+  let xmlContent = `<?xml version='1.0'?>\n<workbook>\n  <preferences>\n    <color-palette name="${paletteName}" type="regular">\n`;
+
+  colors.forEach(function(color) {
+    const trimmedColor = color.trim();
+    xmlContent += `      <color>#${trimmedColor}</color>\n`;
+  });
+
+  xmlContent += `    </color-palette>\n  </preferences>\n</workbook>`;
+
+  // Create a Blob from the XML content
+  const blob = new Blob([xmlContent], { type: 'text/xml' });
+
+  // Set href and download attributes for the link
+  link.href = window.URL.createObjectURL(blob);
+  link.download = 'Preferences.tps';
+
+  // Simulate a click on the link to trigger the download
+  link.click();
 });
